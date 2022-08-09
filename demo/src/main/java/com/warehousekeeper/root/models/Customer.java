bd93f01dbf400465be56f9b43a6e142a327629f6
dilller <dmytropomyluiko@gmail.com>
2022-08-10 01:15:28 +0300

    add documentation comments into customer class

--- demo/src/main/java/com/warehousekeeper/root/models/Customer.java
+++ demo/src/main/java/com/warehousekeeper/root/models/Customer.java
@@ -8,36 +8,52 @@ import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-import java.util.Date;
 import java.util.List;
-
+/**
+ * @author Dmytro
+ * @version 1.0
+ * This class used as entity of table of DB. It describes all customers of Storage
+ */
 @Entity
 @Table(name = "Customer")
 public class Customer {
+    /**
+     * This is int field of DB where store id
+     */
     @Id
     @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-
+    /**
+     * This is String field where store full name
+     */
     @Column(name = "full_name")
     @NotEmpty(message = "Name should not be empty")
     @Size(min = 2, max = 100, message = "Name should be between 2 and 100 characters")
     private String fullName;
-
+    /**
+     * This is int field where store year of birth
+     */
     @Column(name = "year_of_birth")
     @NotNull(message = "Year of birth should not be empty")
     @Min(value = 1951, message = "Year of birth should be greater than 1951")
     private int yearOfBirth;
-
+    /**
+     * This is int field where store phone number
+     */
     @Column(name = "phone_number")
     @NotNull(message = "Year of birth should not be empty")
     private int phoneNumber;
-
+    /**
+     * This is int field where store email
+     */
     @Column(name = "email")
     @NotEmpty(message = "Email should not be empty")
     @Email(message = "You should write valid email")
     private String email;
-
+    /**
+     * This is field storages where store list of storages
+     */
     @OneToMany(mappedBy = "owner", fetch = FetchType.LAZY)
     private List<Storage> storages;
 
